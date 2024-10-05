c26873757001640cf73442d39ee882cd9494fe20
Adan Marques <adan__marx@hotmail.com>
2024-10-04 21:02:34 -0300

    feat(p1-pdmn): cria componente principal e faz hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <span class="border border-dark rounded text-center mt-4">
+          <div className='col-12'>
+            <h1>
+              Hello, tarefas
+            </h1>
+          </div>
+        </span>
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
